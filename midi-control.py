797f773861f34f6e2130a9cb5b9056e8ef22cac4
0cc4m <picard12@live.de>
2021-05-06 17:23:17 +0200

    Toggle handler, rework led setting
    Co-authored-by: Nico <ntw@posteo.net>

--- midi-control.py
+++ midi-control.py
@@ -4,6 +4,17 @@ import yaml
 import subprocess
 
 handlers = {}
+states = {}
+
+control_by_name = {}
+note_on_values = {
+    False: 0,
+    True: 127,
+}
+control_change_values = {
+    "cw": 1,
+    "ccw": 64,
+}
 
 
 def action_handler(name):
@@ -13,26 +24,47 @@ def action_handler(name):
     return handler
 
 
-def handle_actions(midi_port, event, actions):
+def handle_actions(name, event, actions, midi_out):
     for action, options in actions.items():
         action_type = options["type"]
         # event is press, release, cw, ccw
         if action == event:
-            handlers[action_type](options)
+            if name not in states:
+                states[name] = {}
+            handlers[action_type](options, states[name], midi_out)
 
-        for msg in options.get("state_messages", []):
-            print("Sending message: ", msg)
-            midi_port.send(msg)
+
+def set_led(port, name, value):
+    msg = mido.Message(
+        "note_on", note=control_by_name[name]["id"],
+        velocity=note_on_values[value],
+    )
+    port.send(msg)
 
 
 @action_handler("command")
-def command_action(options):
+def command_action(options, state, midi_out):
     subprocess.run(options["command"])
+    for led, value in options.get("states", {}).items():
+        set_led(midi_out, led, value)
+
+
+@action_handler("toggle")
+def toggle_action(options, state, midi_out):
+    if "toggle_state" not in state or not state["toggle_state"]:
+        subprocess.run(options["command_on"])
+        for led, value in options.get("states_on", {}).items():
+            set_led(midi_out, led, value)
+        state["toggle_state"] = True
+    else:
+        subprocess.run(options["command_off"])
+        for led, value in options.get("states_off", {}).items():
+            set_led(midi_out, led, value)
+        state["toggle_state"] = False
 
 
 def process_actions(actions, device):
     # Create dict to enable input lookup by name
-    control_by_name = {}
     for type_name, control_type in device.items():
         if type(control_type) != dict:
             continue
@@ -40,26 +72,6 @@ def process_actions(actions, device):
         for b_id, b_name in control_type.items():
             by_name.update({b_name: {"id": b_id, "type": type_name}})
         control_by_name.update(by_name)
-
-    # Prepare messages for state updates
-    # Iterate all controls
-    for action_control, action_mapping in actions.items():
-        # Iteare all actions mapped to this control
-        for action, options in action_mapping.items():
-            actions[action_control][action]["state_messages"] = []
-            # Iterate all controls which states should be update when
-            # this actions occurs
-            for state_control, state in options.get("states", {}).items():
-                control_info = control_by_name[state_control]
-                # Check which type of control is updated
-                # in order to prepare the correct midi message
-                if control_info["type"] == "note_on":
-                    msg = mido.Message("note_on", note=control_info["id"])
-                    if state:
-                        msg.velocity = device["button_on"]
-                    else:
-                        msg.velocity = device["button_off"]
-                    actions[action_control][action]["state_messages"].append(msg)
 
 
 def main():
@@ -79,11 +91,14 @@ def main():
 
     process_actions(actions, device)
 
-
-    off = device.get("button_off", 0)
-    on = device.get("button_on", 127)
-    cw = device.get("cw", 1)
-    ccw = device.get("ccw", 64)
+    if "button_off" in device:
+        note_on_values[False] = device["button_off"]
+    if "button_on" in device:
+        note_on_values[True] = device["button_on"]
+    if "cw" in device:
+        control_change_values["cw"] = device["cw"]
+    if "ccw" in device:
+        control_change_values["ccw"] = device["ccw"]
 
     port = mido.open_input(midi_device)
 
@@ -91,13 +106,16 @@ def main():
         for msg in port:
             if msg.type == "note_on":
                 name = device.get("note_on", {}).get(msg.note, "unknown")
-                value = "press" if msg.velocity == on else "release"
-                handle_actions(outport, value, actions.get(name, {}))
+                value = "press" if msg.velocity == note_on_values[True]\
+                        else "release"
+                handle_actions(name, value, actions.get(name, {}), outport)
                 print(f"{name}: {value}")
             elif msg.type == "control_change":
-                name = device.get("control_change", {}).get(msg.control, "unknown")
-                value = "cw" if msg.value == cw else "ccw"
-                handle_actions(outport, value, actions.get(name, {}))
+                name = device.get("control_change", {}).get(msg.control,
+                                                            "unknown")
+                value = "cw" if msg.value == control_change_values["cw"]\
+                        else "ccw"
+                handle_actions(name, value, actions.get(name, {}), outport)
                 print(f"{name}: {value}")
             elif msg.type == "pitchwheel":
                 name = "Pitch"
