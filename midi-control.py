10ea1464ed2d4d7d7c855a89027b41d0f045f0cf
Nico <ntw@posteo.net>
2021-05-06 08:47:33 +0200

    Enable defintion of led states in action

--- midi-control.py
+++ midi-control.py
@@ -13,16 +13,53 @@ def action_handler(name):
     return handler
 
 
-def handle_actions(event, actions):
+def handle_actions(midi_port, event, actions):
     for action, options in actions.items():
         action_type = options["type"]
+        # event is press, release, cw, ccw
         if action == event:
             handlers[action_type](options)
+
+        for msg in options.get("state_messages", []):
+            print("Sending message: ", msg)
+            midi_port.send(msg)
 
 
 @action_handler("command")
 def command_action(options):
     subprocess.run(options["command"])
+
+
+def process_actions(actions, device):
+    # Create dict to enable input lookup by name
+    control_by_name = {}
+    for type_name, control_type in device.items():
+        if type(control_type) != dict:
+            continue
+        by_name = {}
+        for b_id, b_name in control_type.items():
+            by_name.update({b_name: {"id": b_id, "type": type_name}})
+        control_by_name.update(by_name)
+
+    # Prepare messages for state updates
+    # Iterate all controls
+    for action_control, action_mapping in actions.items():
+        # Iteare all actions mapped to this control
+        for action, options in action_mapping.items():
+            actions[action_control][action]["state_messages"] = []
+            # Iterate all controls which states should be update when
+            # this actions occurs
+            for state_control, state in options.get("states", {}).items():
+                control_info = control_by_name[state_control]
+                # Check which type of control is updated
+                # in order to prepare the correct midi message
+                if control_info["type"] == "note_on":
+                    msg = mido.Message("note_on", note=control_info["id"])
+                    if state:
+                        msg.velocity = device["button_on"]
+                    else:
+                        msg.velocity = device["button_off"]
+                    actions[action_control][action]["state_messages"].append(msg)
 
 
 def main():
@@ -40,6 +77,8 @@ def main():
         all_actions = yaml.load(f.read(), Loader=yaml.SafeLoader)
         actions = all_actions.get(midi_device, {})
 
+    process_actions(actions, device)
+
 
     off = device.get("button_off", 0)
     on = device.get("button_on", 127)
@@ -53,13 +92,12 @@ def main():
             if msg.type == "note_on":
                 name = device.get("note_on", {}).get(msg.note, "unknown")
                 value = "press" if msg.velocity == on else "release"
-                handle_actions(value, actions.get(name, {}))
+                handle_actions(outport, value, actions.get(name, {}))
                 print(f"{name}: {value}")
-                outport.send(msg)
             elif msg.type == "control_change":
                 name = device.get("control_change", {}).get(msg.control, "unknown")
                 value = "cw" if msg.value == cw else "ccw"
-                handle_actions(value, actions.get(name, {}))
+                handle_actions(outport, value, actions.get(name, {}))
                 print(f"{name}: {value}")
             elif msg.type == "pitchwheel":
                 name = "Pitch"
