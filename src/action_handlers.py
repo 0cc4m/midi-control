d103d9a21aa28f414a5fe8ff7660b32f5796cc5b
Nico <ntw@posteo.net>
2021-05-09 12:34:45 +0200

    Move action handlers to separate file
    Convert action handlers to classes

---
+++ src/action_handlers.py
@@ -0,0 +1,145 @@
+import dbus
+import subprocess
+import mido
+import logging
+from dbus import SessionBus
+from dbus.exceptions import DBusException
+
+log = logging.getLogger("midi-control")
+
+handler_classes = {}
+
+def action_handler(name):
+    def handler(handler_class):
+        handler_classes[name] = handler_class
+        return handler_class
+
+    return handler
+
+
+class ActionHandler:
+    def __init__(self, options, controls, consts, midi_out):
+        self.options = options
+        self.controls = controls
+        self.consts = consts
+        self.midi_out = midi_out
+
+    def __call__(self, msg):
+        pass
+
+    def set_led(self, name, value):
+        msg = mido.Message(
+            "note_on",
+            note=self.controls[name]["id"],
+            velocity=self.consts["note_on_values"][value],
+        )
+        self.midi_out.send(msg)
+
+
+@action_handler("command")
+class CommandAction(ActionHandler):
+    def __call__(self, msg):
+        subprocess.run(self.options["command"])
+        for led, value in self.options.get("states", {}).items():
+            self.set_led(led, value)
+
+
+@action_handler("toggle")
+class ToggleAction(ActionHandler):
+    def __init__(self, options, controls, consts, midi_out):
+        super().__init__(options, controls, consts, midi_out)
+        self.state = False
+
+    def __call__(self, msg):
+        if self.state:
+            subprocess.run(self.options["command_on"])
+            for led, value in self.options.get("states_on", {}).items():
+                self.set_led(led, value)
+            self.state = False
+        else:
+            subprocess.run(self.options["command_off"])
+            for led, value in self.options.get("states_off", {}).items():
+                self.set_led(led, value)
+            self.state = True
+
+
+@action_handler("fader_command")
+class FaderCommandAction(ActionHandler):
+    def __call__(self, msg):
+        minv = self.options["min"]
+        maxv = self.options["max"]
+        val = msg.bin()[2]
+
+        final = int(round(minv + val / 127 * maxv, 0))
+
+        command = self.options["command"].copy()
+        command = [s.replace("$VALUE", str(final)) for s in command]
+
+        subprocess.run(command)
+
+
+@action_handler("dbus")
+class DBusAction(ActionHandler):
+    def __init__(self, options, controls, consts, midi_out):
+        super().__init__(options, controls, consts, midi_out)
+        self.dbus_method = False
+
+    def __call__(self, msg):
+        method_name = self.options["method"]
+        service = self.options["service"]
+        path = self.options["path"]
+        if not self.dbus_method:
+            try:
+                proxy = SessionBus().get_object(service, path)
+                method = proxy.get_dbus_method(method_name)
+                self.dbus_method = method
+            except DBusException as e:
+                log.error(
+                    "Could not create dbus proxy in dbus action: %s"
+                    % e.get_dbus_message()
+                )
+                return
+
+        args = self.options.get("args", [])
+
+        self.dbus_method(*args)
+
+        for led, value in self.options.get("states", {}).items():
+            self.set_led(led, value)
+
+
+@action_handler("dbus_toggle")
+class DBusToggleAction(ActionHandler):
+    def __init__(self, options, controls, consts, midi_out):
+        super().__init__(options, controls, consts, midi_out)
+        self.dbus_method = False
+        self.state = False
+
+    def __call__(self, msg):
+        method_name = self.options["method"]
+        service = self.options["service"]
+        path = self.options["path"]
+        if not self.dbus_method:
+            try:
+                proxy = SessionBus().get_object(service, path)
+                method = proxy.get_dbus_method(method_name)
+                self.dbus_method = method
+            except DBusException as e:
+                log.error(
+                    "Could not create dbus proxy in dbus action: %s"
+                    % e.get_dbus_message()
+                )
+                return
+
+        if self.state:
+            args = self.options.get("args_on", [])
+            self.state = False
+            for led, value in self.options.get("states_on", {}).items():
+                self.set_led(led, value)
+        else:
+            args = self.options.get("args_off", [])
+            self.state = True
+            for led, value in self.options.get("states_off", {}).items():
+                self.set_led(led, value)
+
+        self.dbus_method(*args)
--- src/midi_control.py
+++ src/midi_control.py
@@ -16,148 +16,49 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
+import os
+import time
 import argparse
 import logging
 import mido
-import os
-import subprocess
-import time
 import yaml
-from dbus import SessionBus
-from dbus.exceptions import DBusException
+from action_handlers import handler_classes
 
-log = logging.getLogger("midi-controller")
+log = logging.getLogger("midi-control")
 
 handlers = {}
-states = {}
 
 control_by_name = {}
-note_on_values = {
-    False: 0,
-    True: 127,
-}
-control_change_values = {
-    "cw": 1,
-    "ccw": 64,
-}
 
-
-def action_handler(name):
-    def handler(func):
-        handlers[name] = func
-        return func
-    return handler
+consts = {
+    "note_on_values": {
+        False: 0,
+        True: 127,
+    },
+    "control_change_values": {
+        "cw": 1,
+        "ccw": 64,
+    },
+}
 
 
 def handle_actions(name, value, actions, midi_out, msg):
-    if name not in states:
-        states[name] = {}
+    if name not in handlers:
+        handlers[name] = {}
     for action, options in actions.items():
         action_type = options["type"]
-        if ((action == "press" and value == note_on_values[True]) or
-                (action == "release" and value == note_on_values[False]) or
-                (action == "cw" and value == control_change_values["cw"]) or
-                (action == "ccw" and value == control_change_values["ccw"]) or
-                action == "set"):
-            handlers[action_type](options, states[name], midi_out, msg)
-
-
-def set_led(port, name, value):
-    msg = mido.Message(
-        "note_on", note=control_by_name[name]["id"],
-        velocity=note_on_values[value],
-    )
-    port.send(msg)
-
-
-@action_handler("command")
-def command_action(options, state, midi_out, msg):
-    subprocess.run(options["command"])
-    for led, value in options.get("states", {}).items():
-        set_led(midi_out, led, value)
-
-
-@action_handler("toggle")
-def toggle_action(options, state, midi_out, msg):
-    if "toggle_state" not in state or not state["toggle_state"]:
-        subprocess.run(options["command_on"])
-        for led, value in options.get("states_on", {}).items():
-            set_led(midi_out, led, value)
-        state["toggle_state"] = True
-    else:
-        subprocess.run(options["command_off"])
-        for led, value in options.get("states_off", {}).items():
-            set_led(midi_out, led, value)
-        state["toggle_state"] = False
-
-
-@action_handler("fader_command")
-def fader_command_action(options, state, midi_out, msg):
-    minv = options["min"]
-    maxv = options["max"]
-    val = msg.bin()[2]
-
-    final = int(round(minv + val / 127 * maxv, 0))
-
-    command = options["command"].copy()
-
-    for i in range(0, len(command)):
-        command[i] = command[i].replace("$VALUE", str(final))
-
-    subprocess.run(command)
-
-
-@action_handler("dbus")
-def dbus_action(options, state, midi_out, msg):
-    method_name = options["method"]
-    service = options["service"]
-    path = options["path"]
-    if "dbus" not in state:
-        try:
-            proxy = SessionBus().get_object(service, path)
-            method = proxy.get_dbus_method(method_name)
-            state["dbus"] = method
-        except DBusException as e:
-            log.error("Could not create dbus proxy in dbus action: %s"
-                      % e.get_dbus_message())
-            return
-
-    args = options.get("args", [])
-
-    state["dbus"](*args)
-
-    for led, value in options.get("states", {}).items():
-        set_led(midi_out, led, value)
-
-
-@action_handler("dbus_toggle")
-def dbus_toggle_action(options, state, midi_out, msg):
-    method_name = options["method"]
-    service = options["service"]
-    path = options["path"]
-    if "dbus_toggle" not in state:
-        try:
-            proxy = SessionBus().get_object(service, path)
-            method = proxy.get_dbus_method(method_name)
-            state["dbus_toggle"] = {"method": method,
-                                    "state": False}
-        except DBusException as e:
-            log.error("Could not create dbus proxy in dbus_toggle action: %s"
-                      % e.get_dbus_message())
-            return
-
-    if state["dbus_toggle"]["state"]:
-        args = options.get("args_on", [])
-        state["dbus_toggle"]["state"] = False
-        for led, value in options.get("states_on", {}).items():
-            set_led(midi_out, led, value)
-    else:
-        args = options.get("args_off", [])
-        state["dbus_toggle"]["state"] = True
-        for led, value in options.get("states_off", {}).items():
-            set_led(midi_out, led, value)
-
-    state["dbus_toggle"]["method"](*args)
+        if action not in handlers[name]:
+            handlers[name][action] = handler_classes[action_type](
+                options, control_by_name, consts, midi_out
+            )
+        if (
+            (action == "press" and value == consts["note_on_values"][True])
+            or (action == "release" and value == consts["note_on_values"][False])
+            or (action == "cw" and value == consts["control_change_values"]["cw"])
+            or (action == "ccw" and value == consts["control_change_values"]["ccw"])
+            or action == "set"
+        ):
+            handlers[name][action](msg)
 
 
 def map_controls(devices):
@@ -171,11 +72,8 @@ def map_controls(devices):
             by_name = {}
             for b_id, b_name in control_type.items():
                 by_name.update(
-                        {b_name: {
-                            "id": b_id,
-                            "type": type_name,
-                            "device": device_name}}
-                        )
+                    {b_name: {"id": b_id, "type": type_name, "device": device_name}}
+                )
             control_by_name.update(by_name)
 
 
@@ -186,13 +84,11 @@ def handle_messages(inport, outport, device, actions):
             value = msg.velocity
             log.info(f"{name}: {value}")
         elif msg.type == "control_change":
-            name = device.get("control_change", {}).get(msg.control,
-                                                        "unknown")
+            name = device.get("control_change", {}).get(msg.control, "unknown")
             value = msg.value
             log.info(f"{name}: {value}")
         elif msg.type == "pitchwheel":
-            name = device.get("pitchwheel", {})\
-                .get(msg.bin()[0], "unknown")
+            name = device.get("pitchwheel", {}).get(msg.bin()[0], "unknown")
             channel = msg.channel
             value = msg.pitch
             log.info(f"{name} {channel}: {value}")
@@ -200,23 +96,30 @@ def handle_messages(inport, outport, device, actions):
             continue
 
         handle_actions(
-            name, value, actions.get(name, {}), outport, msg,
+            name,
+            value,
+            actions.get(name, {}),
+            outport,
+            msg,
         )
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--device-file",
-                        default="~/.config/midi-control/device.yml")
-    parser.add_argument("-a", "--actions-file",
-                        default="~/.config/midi-control/actions.yml")
-    parser.add_argument("--log", "-l",
-                        default="WARNING",
-                        help="Set loglevel")
+    parser.add_argument(
+        "-d", "--device-file", default="~/.config/midi-control/device.yml"
+    )
+    parser.add_argument(
+        "-a", "--actions-file", default="~/.config/midi-control/actions.yml"
+    )
+    parser.add_argument("--log", "-l", default="WARNING", help="Set loglevel")
     args = parser.parse_args()
 
-    logging.basicConfig(format="[%(levelname)s]: %(message)s",
-                        level=args.log.upper())
+    formatter = logging.Formatter(fmt="[%(levelname)s]: %(message)s")
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    log.addHandler(handler)
+    log.setLevel(args.log.upper())
 
     with open(os.path.expanduser(args.device_file), "r") as f:
         devices = yaml.load(f.read(), Loader=yaml.SafeLoader)
@@ -227,15 +130,15 @@ def main():
     map_controls(devices)
 
     if "consts" in devices:
-        consts = devices["consts"]
+        device_consts = devices["consts"]
         if "button_off" in consts:
-            note_on_values[False] = consts["button_off"]
+            consts["note_on_values"][False] = device_consts["button_off"]
         if "button_on" in consts:
-            note_on_values[True] = consts["button_on"]
+            consts["note_on_values"][True] = device_consts["button_on"]
         if "cw" in consts:
-            control_change_values["cw"] = consts["cw"]
+            consts["control_change_values"]["cw"] = device_consts["cw"]
         if "ccw" in consts:
-            control_change_values["ccw"] = consts["ccw"]
+            consts["control_change_values"]["ccw"] = device_consts["ccw"]
         devices.pop("consts")
 
     device_ports = {}
@@ -246,10 +149,7 @@ def main():
 
     while True:
         for device, ports in device_ports.items():
-            handle_messages(ports["in"],
-                            ports["out"],
-                            devices[device],
-                            actions[device])
+            handle_messages(ports["in"], ports["out"], devices[device], actions[device])
             time.sleep(0.01)
 
 
